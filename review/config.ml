1b8e133cfce3e872913126920b68ca6556a61ad1
Qi LI <liqi0425@gmail.com>
2016-07-01 00:57:32 +0100

    using mirge http_server device

--- review/config.ml
+++ review/config.ml
@@ -1,47 +1,27 @@
-(* Copyright (C) 2015, Thomas Leonard
- * See the README file for details. *)
-
 open Mirage
 
-let ipv4_conf =
-  let i = Ipaddr.V4.of_string_exn in
-  {
-    address  = i "10.0.0.2";
-    netmask  = i "255.255.255.0";
-    gateways = [i "10.0.0.1"];
-  }
-
-(*
-let direct_stack_impl console () =
-  let open Key in
-  if_impl (value @@ dhcp ())
-    (direct_stackv4_with_dhcp console tap0)
-    (direct_stackv4_with_static_ipv4 console tap0 ipv4_conf)
-*)
-(*
 let stack =
-  let open Key in
-  if_impl is_xen
-    (generic_stackv4 default_console tap0)
-    (socket_stackv4 default_console [Ipaddr.V4.any])
-*)
+  match get_mode () with
+  | `Xen -> generic_stackv4 default_console tap0
+  | _ -> socket_stackv4 default_console [Ipaddr.V4.any]
 
-let stack =
-  let open Key in
-  if_impl is_xen
-          (direct_stackv4_with_static_ipv4 default_console tap0 ipv4_conf)
-          (socket_stackv4 default_console [Ipaddr.V4.any])
+let keys = generic_kv_ro "tls"
+
+let https = http_server @@ conduit_direct ~tls:true stack
 
 let main =
-  let libraries = [
-    "logs";
-    "mirage-http";
-    "mirage-logs";
-    "git";
-    "irmin.mirage"] in
-  foreign ~libraries ~deps:[abstract nocrypto] "Unikernel.Main" (stackv4 @-> clock @-> job)
+  foreign "Unikernel.Main"
+    (http @-> kv_ro @-> clock @-> job)
+
+let tracing = mprof_trace ~size:1000000 ()
 
 let () =
-  register "review" [
-    main $ stack $ default_clock
+  let libraries = [
+      "logs";
+      "mirage-logs";
+      "git";
+      "irmin.mirage";
+    ] in
+  register ~libraries "review" [
+    main $ https $ keys $ default_clock
   ]
--- review/unikernel.ml
+++ review/unikernel.ml
@@ -1,43 +1,19 @@
-open Lwt
+open V1
 open V1_LWT
+open Lwt
+open Result
 
+let src = Logs.Src.create "ucn.review"
+module Log = (val Logs.src_log src : Logs.LOG)
 
-module Main (Stack:STACKV4) (Clock:V1.CLOCK) = struct
-
-  module TCP  = Stack.TCPV4
-  module Server = Cohttp_mirage.Server(TCP)
+module Store = struct
 
   module Context = struct let v () = return_none end
   module Mirage_git_memory = Irmin_mirage.Irmin_git.Memory(Context)(Git.Inflate.None)
-  module Store = Mirage_git_memory(Irmin.Contents.Json)(Irmin.Ref.String)(Irmin.Hash.SHA1)
-
-  let owner = "ucn.review"
-  let task s = Irmin.Task.create ~date:0L ~owner s
-  let config = Irmin_mem.config ()
-
-  let src = Logs.Src.create owner
-  module Log = (val Logs.src_log src : Logs.LOG)
-  module Logs_reporter = Mirage_logs.Make(Clock)
-
-  let headers =
-    let hdr = Cohttp.Header.init () in
-    Cohttp.Header.add hdr "Access-Control-Allow-Origin" "*" (* make chrome happy *)
+  module S = Mirage_git_memory(Irmin.Contents.Json)(Irmin.Ref.String)(Irmin.Hash.SHA1)
 
   let data_path = ["data"]
   let log_path = ["logs"]
-
-  let list_reviews s body =
-    let t = "list id of reviewed movies" in
-    Log.app (fun msgf -> msgf "%s" t);
-    let s = s t in
-    let p = data_path in
-    Store.list s p >>= fun keys ->
-    let id_lst = List.map (fun key -> key |> List.rev |> List.hd) keys in
-    Log.info (fun msgf -> msgf "respond: [%s]" (String.concat " " id_lst));
-    let arr = Ezjsonm.(list string id_lst) in
-    let body = Cohttp_lwt_body.of_string (Ezjsonm.to_string arr) in
-    Server.respond ~headers ~status:`OK ~body ()
-
 
   let check_fields = function
     | `O _ as d ->
@@ -52,50 +28,46 @@ module Main (Stack:STACKV4) (Clock:V1.CLOCK) = struct
        fail_invalid_arg "not json object"
 
 
-  let update_review path s body =
-    let id = List.hd path in
-    let t = "create/update new review " ^ id in
+  let read s id =
+    let t = "read review " ^ id in
     Log.app (fun msgf -> msgf "%s" t);
+    let s = s t in
+    let p = data_path @ [id] in
+    S.read s p >>= function
+    | None ->
+       Log.info (fun f -> f "not found review %s" id);
+       return (Error Not_found)
+    | Some v ->
+       let v = Ezjsonm.to_string v in
+       Log.info (fun f -> f "read_review respond: %s" v);
+       return (Ok v)
+
+
+  let update s id body =
+    let t = "create/update new review " ^ id in
+    Log.app (fun f -> f "%s" t);
     let s = s t in
     let p = data_path @ [id] in
     catch (fun () ->
       Cohttp_lwt_body.to_string body >>= fun body ->
       let v = Ezjsonm.from_string body in
       check_fields v >>= fun () ->
-      Store.update s p v >>= fun () ->
-      Log.info (fun msgf -> msgf "review created/updated");
-      Server.respond ~status:`OK ~headers ~body:Cohttp_lwt_body.empty ()
+      S.update s p v >>= fun () ->
+      Log.info (fun f -> f "review created/updated");
+      return (Ok "")
       ) (fun e ->
-      Log.app (fun msgf -> msgf "exn: %s" (Printexc.to_string e));
-      Server.respond ~status:`Bad_request ~headers ~body:Cohttp_lwt_body.empty ())
+      Log.app (fun f -> f "exn: %s" (Printexc.to_string e));
+      return (Error e))
 
 
-  let read_review path s body =
-    let id = List.hd path in
-    let t = "read review " ^ id in
-    Log.app (fun msgf -> msgf "%s" t);
-    let s = s t in
-    let p = data_path @ [id] in
-    Store.read s p >>= function
-    | None ->
-       Log.info (fun msgf -> msgf "not found review %s" id);
-       Server.respond_error ~headers ~status:`Not_found ~body:"" ()
-    | Some v ->
-       let v = Ezjsonm.to_string v in
-       Log.info (fun msgf -> msgf "respond: %s" v);
-       let body = Cohttp_lwt_body.of_string v in
-       Server.respond ~headers ~status:`OK ~body ()
-
-
-  let remove_review path s body =
-    let id = List.hd path in
+  let remove s id =
     let t = "remove review " ^ id in
-    Log.app (fun msgf -> msgf "%s" t);
+    Log.app (fun f -> f "%s" t);
     let s = s t in
     let p = data_path @ [id] in
-    Store.remove s p >>= fun () ->
-    Log.info (fun msgf -> msgf "remove review %s" id);
-    Server.respond ~headers ~status:`OK ~body:Cohttp_lwt_body.empty ()
+    S.remove s p >>= fun () ->
+    Log.info (fun f -> f "remove review %s" id);
+    return (Ok "")
 
 
   let meta_of_review = function
@@ -106,54 +78,128 @@ module Main (Stack:STACKV4) (Clock:V1.CLOCK) = struct
     | _ -> Lwt.fail_with "never"
 
 
-  let read_meta_review path s body =
-    let id = List.hd path in
-    let t = "read metadata of a review " ^ id in
-    Log.app (fun msgf -> msgf "%s" t);
+  let get_meta s id =
+    let t = "get metadata of a review " ^ id in
+    Log.app (fun f -> f "%s" t);
     let s = s t in
     let p = data_path @ [id] in
-    Store.read s p >>= function
+    S.read s p >>= function
     | None ->
-       Log.info (fun msgf -> msgf "not found review %s" id);
-       Server.respond_error ~headers ~status:`Not_found ~body:"" ()
+       Log.info (fun f -> f "not found review %s for metadata" id);
+       return (Error Not_found)
     | Some v ->
        meta_of_review v >>= fun meta ->
        Log.info (fun msgf -> msgf "respond metadata %s" meta);
-       let body = Cohttp_lwt_body.of_string meta in
-       Server.respond ~headers ~status:`OK ~body ()
+       return (Ok meta)
 
 
-  let dispatch request =
-    let path =
-      Cohttp.Request.uri request
-      |> Uri.path
-      |> Astring.String.cuts ~empty:false ~sep:"/" in
-    match path with
-    | "create" :: tl -> update_review tl
-    | "read" :: tl -> read_review tl
-    | "update" :: tl -> update_review tl
-    | "delete" :: tl -> remove_review tl
-    | "meta" :: tl -> read_meta_review tl
-    | ["list"] -> list_reviews
-    | _ ->
-       fun _ _ ->
-       Server.respond_error ~status:`Bad_request ~headers ~body:"" ()
+  let list s =
+    let t = "list id of reviewed movies" in
+    Log.app (fun f -> f "%s" t);
+    let s = s t in
+    let p = data_path in
+    S.list s p >>= fun keys ->
+    let id_lst = List.map (fun key -> key |> List.rev |> List.hd) keys in
+    Log.info (fun f -> f "list respond: [%s]" (String.concat " " id_lst));
+    let arr = Ezjsonm.(list string id_lst) in
+    return (Ok (Ezjsonm.to_string arr))
 
 
-  let handle_request s (flow, conn) request body =
-    let ip, port = TCP.get_dest flow in
-    Log.app (fun msgf -> msgf "connection %s from %s:%d"
-      (Cohttp.Connection.to_string conn) (Ipaddr.V4.to_string ip) port);
-    dispatch request s body
+  let dispatch store body steps =
+    match steps with
+    | ["create"; id] -> update store id body
+    | ["read"; id] -> read store id
+    | ["update"; id] -> update store id body
+    | ["delete"; id] -> remove store id
+    | ["meta"; id] -> get_meta store id
+    | ["list"] -> list store
+    | _ -> return (Error Not_found)
 
 
-  let start stack clock () =
-    (*Logs.set_reporter (Utils.src_stamp_reporter ());
-    Logs.set_level (Some Logs.Info);*)
-    Logs_reporter.(create () |> set_reporter);
-    Store.Repo.create config >>= fun repo ->
-    Store.master task repo >>= fun s ->
-    let http = Server.make ~conn_closed:ignore ~callback:(handle_request s) () in
-    Stack.listen_tcpv4 stack ~port:8081 (Server.listen http);
-    Stack.listen stack
+  let init () =
+    let task s = Irmin.Task.create ~date:0L ~owner:"ucn.review" s in
+    let config = Irmin_mem.config () in
+    S.Repo.create config >>= fun repo ->
+    S.master task repo
+end
+
+
+module Dispatch
+    (Http: Cohttp_lwt.Server) = struct
+
+  let headers = Cohttp.Header.init_with
+    "Strict-Transport-Security" "max-age=31536000"
+  let empty_body = Cohttp_lwt_body.empty
+
+
+  let review_dispatcher store uri req body =
+    let p = Uri.path uri in
+    let steps = Astring.String.cuts ~empty:false ~sep:"/" p in
+    Store.dispatch store body steps
+    >>= function
+    | Ok "" -> Http.respond ~headers ~status:`OK ~body:empty_body ()
+    | Ok json ->
+       let headers = Cohttp.Header.add headers
+         "content-type" "application/json" in
+       let body = Cohttp_lwt_body.of_string json in
+       Http.respond ~headers ~status:`OK ~body ()
+    | Error e ->
+       let body = Printexc.to_string e |> Cohttp_lwt_body.of_string in
+       Http.respond ~headers ~status:`Not_found ~body ()
+
+
+  let redirect uri _req _body =
+    let new_uri = Uri.with_scheme uri (Some "https") in
+    let new_uri = Uri.with_port new_uri (Some 4433) in
+    let headers =
+      Cohttp.Header.add headers "location" (Uri.to_string new_uri)
+    in
+    Http.respond ~headers ~status:`Moved_permanently ~body:`Empty ()
+
+
+  let serve dispatch =
+    let callback (_, cid) request body =
+      let uri = Cohttp.Request.uri request in
+      let cid = Cohttp.Connection.to_string cid in
+      Log.info (fun f -> f  "[%s] serving %s." cid (Uri.to_string uri));
+      dispatch uri request body
+    in
+    let conn_closed (_,cid) =
+      let cid = Cohttp.Connection.to_string cid in
+      Log.info (fun f -> f "[%s] closing" cid);
+    in
+    Http.make ~conn_closed ~callback ()
+end
+
+
+module Main
+     (Http: Cohttp_lwt.Server)
+     (Keys: KV_RO)
+     (Clock: V1.CLOCK) = struct
+
+  module X509 = Tls_mirage.X509(Keys)(Clock)
+
+  module D = Dispatch(Http)
+
+  module Logs_reporter = Mirage_logs.Make(Clock)
+
+  let tls_init kv =
+    X509.certificate kv `Default >>= fun cert ->
+    let conf = Tls.Config.server ~certificates:(`Single cert) () in
+    Lwt.return conf
+
+  let start http keys _clock =
+    Logs.(set_level (Some Info));
+    Logs_reporter.(create () |> run) @@ fun () ->
+
+    tls_init keys >>= fun cfg ->
+    let tcp = `TCP 4433 in
+    let tls = `TLS (cfg, tcp) in
+
+    Store.init () >>= fun store ->
+
+    Lwt.join [
+      http tls @@ D.serve (D.review_dispatcher store);
+      http (`TCP 8081) @@ D.serve D.redirect
+    ]
 end
